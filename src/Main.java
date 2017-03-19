32a2590f501e7618ba4381679d6c50792a17b60c
Joshua Torres <torresj1997@outlook.com>
2017-03-19 14:20:34 -0500

    Driver (Main) class

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------------------
+// 
+// Created by Josh on 3/19/2017.
+//
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Board board = new Board();
+    }
+}
