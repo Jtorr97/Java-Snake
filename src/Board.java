ec6b6c270a4143cb373c6db6cdfddc32cae84aa3
Joshua Torres <torresj1997@outlook.com>
2017-03-19 14:22:04 -0500

    Snake graphics and input added
    - Next is to add a collision checker, and spawn the food.
    - Score also need to be implemented after both of these implemented.

--- src/Board.java
+++ src/Board.java
@@ -6,22 +6,32 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.*;
 
-public class Board extends JPanel
+public class Board extends JPanel implements KeyListener
 {
-
+    private volatile Snake gameSnake = new Snake();
     private JFrame gameFrame = new JFrame();
+    private final int WINDOW_WIDTH = 700;
+    private final int WINDOW_HEIGHT = 550;
+    private int speed = 100;
+    private int score;
 
-    private Board()
+    Board()
     {
+        // Create the main window
         gameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        gameFrame.addKeyListener(this);
         gameFrame.setTitle("Snake");
-        gameFrame.setSize(800, 800);
+        gameFrame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         gameFrame.setResizable(false);
+        gameFrame.add(this);
         JMenuBar bar = new JMenuBar();
         JMenu gameMenu = new JMenu("Game");
 
-
+        // New game button
         JMenuItem newgame = new JMenuItem("New Game");
         newgame.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
@@ -30,6 +40,7 @@ public class Board extends JPanel
             }
         } );
 
+        // Various other buttons
         JMenu aboutMenu = new JMenu("About");
         JMenuItem help = new JMenuItem ("Help");
         help.addActionListener(new ActionListener() {
@@ -46,10 +57,115 @@ public class Board extends JPanel
         bar.add(aboutMenu);
         gameFrame.setJMenuBar(bar);
         gameFrame.setVisible(true);
+
+        gameSnake.snakeTimer = new Timer(speed, reDrawSnake);
+        gameSnake.snakeTimer.restart();
     }
 
-    public static void main(String[] args)
+    public void paint(Graphics g)
     {
-        Board board = new Board();
+        // The game board and window
+        Graphics2D graphics2D = (Graphics2D)g;
+        graphics2D.setStroke(new BasicStroke(3));
+        graphics2D.setColor(Color.white);
+        graphics2D.fillRect(0,0,700,500);
+
+        // Title text
+        Font font = new Font("Verdana", Font.BOLD, 40);
+        graphics2D.setFont(font);
+        graphics2D.setColor(Color.BLACK);
+        graphics2D.drawString("Snake!", 50, 40);
+
+        // Score box/text
+        graphics2D.setColor(Color.RED);
+        graphics2D.drawRect(49, 49, 501, 401);
+        graphics2D.setColor(Color.BLACK);
+        graphics2D.fillRect(50, 50, 500, 400);
+        graphics2D.drawRect(560, 50, 125, 40);
+        graphics2D.setFont(new Font("Verdana", Font.PLAIN, 12));
+        graphics2D.drawString("Score: " + score, 565, 75);
+
+        // Draw the snake
+        gameSnake.drawSnake(graphics2D);
+    }
+
+    private Action reDrawSnake = new AbstractAction() {
+        public void actionPerformed(ActionEvent e) {
+            repaint();
+        }
+    };
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e)
+    {
+        // Handle input: Use arrow keys to move the snake
+        switch(e.getKeyCode()) {
+            case KeyEvent.VK_LEFT:
+            {
+                if(gameSnake.direction == 'a')
+                    break;
+                if(gameSnake.direction != 'd') {
+                    gameSnake.snakeTimer.stop();
+                    gameSnake.direction = 'a';
+                    repaint();
+                    gameSnake.snakeTimer = new Timer(speed, reDrawSnake);
+                    gameSnake.snakeTimer.start();
+                }
+                break;
+            }
+
+            case KeyEvent.VK_DOWN:
+            {
+                if(gameSnake.direction == 's')
+                    break;
+                if(gameSnake.direction != 'w') {
+                    gameSnake.snakeTimer.stop();
+                    gameSnake.direction = 's';
+                    repaint();
+                    gameSnake.snakeTimer = new Timer(speed, reDrawSnake);
+                    gameSnake.snakeTimer.start();
+                }
+                break;
+            }
+
+            case KeyEvent.VK_UP:
+            {
+                if(gameSnake.direction == 'w')
+                    break;
+                if(gameSnake.direction != 's') {
+                    gameSnake.snakeTimer.stop();
+                    gameSnake.direction = 'w';
+                    repaint();
+                    gameSnake.snakeTimer = new Timer(speed, reDrawSnake);
+                    gameSnake.snakeTimer.start();
+                }
+                break;
+
+            }
+
+            case KeyEvent.VK_RIGHT:
+            {
+                if(gameSnake.direction == 'd')
+                    break;
+                if(gameSnake.direction != 'a') {
+                    gameSnake.snakeTimer.stop();
+                    gameSnake.direction = 'd';
+                    repaint();
+                    gameSnake.snakeTimer = new Timer(speed, reDrawSnake);
+                    gameSnake.snakeTimer.start();
+                }
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
--- src/Snake.java
+++ src/Snake.java
@@ -3,7 +3,107 @@
 // Snake.java
 //
 
+import java.awt.*;
+import javax.swing.*;
 
-public class Snake
+public class Snake implements Runnable
 {
+    public char direction = 'a';
+    public int[] xArray;
+    public int[] yArray;
+    public int size;
+    boolean gameOver = false;
+    public Timer snakeTimer;
+
+    Snake()
+    {
+        xArray = new int[2000];
+
+        xArray[0] = 300;
+        xArray[1] = 310;
+        xArray[2] = 320;
+        xArray[3] = 330;
+
+
+        yArray = new int[2000];
+        yArray[0] = 250;
+        yArray[1] = 250;
+        yArray[2] = 250;
+        yArray[3] = 250;
+
+
+        size = 4;
+    }
+
+    // Draws the snake onto the screen
+    public void drawSnake(Graphics2D g2)
+    {
+        updateSnake();
+        if(!gameOver)
+        {
+            g2.setColor(Color.GREEN);
+            for(int i = 0; i < size; i++)
+            {
+                g2.fillRect(xArray[i], yArray[i], 10, 10);
+            }
+        }
+        else
+            System.out.println("Lose"); // TODO: Draw a lose message when colliding with wall or self
+    }
+
+    // Snake updated after each direction change
+    public void updateSnake()
+    {
+        if(direction == 'a')
+        {
+            for( int i = size - 1; i > 0; i--)
+            {
+                xArray[i] = xArray[i - 1];
+                yArray[i] = yArray[i - 1];
+            }
+
+            xArray[0] = xArray[0] - 10;
+        }
+
+        if(direction == 's')
+        {
+            for(int i = size - 1; i > 0; i--)
+        {
+            xArray[i] = xArray[i - 1];
+            yArray[i] = yArray[i - 1];
+        }
+
+            yArray[0] = yArray[0] + 10;
+        }
+
+        if(direction == 'd')
+        {
+            for(int i = size - 1; i > 0; i--)
+            {
+                xArray[i] = xArray[i - 1];
+                yArray[i] = yArray[i - 1];
+            }
+
+            xArray[0] = xArray[0] + 10;
+        }
+
+        if(direction == 'w')
+        {
+            for(int i = size - 1; i > 0; i--)
+            {
+                xArray[i] = xArray[i - 1];
+                yArray[i] = yArray[i - 1];
+            }
+
+            yArray[0] = yArray[0] - 10;
+        }
+    }
+
+    // TODO: Create collision checker method, spawn food later
+
+    @Override
+    public void run()
+    {
+
+    }
 }
