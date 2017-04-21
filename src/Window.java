9060188e5f18afb518af35fbafb94c733b39289d
Joshua Torres <torresj1997@outlook.com>
2017-04-21 10:46:23 -0500

    Cleanup

--- src/Window.java
+++ src/Window.java
@@ -1,20 +1,31 @@
 //---------------------------------------------------------------------------------
 // 
-// Created by Josh on 4/19/2017.
+// Window.java
 //
-
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 
-public class Window extends JPanel
+public class Window
 {
     // Main gameframe
     private static JFrame gameFrame = new JFrame();
 
+    private JMenuBar bar = new JMenuBar();
+    private JMenu helpMenu = new JMenu("Help");
+
+    // Various other buttons
+    private JMenuItem howToPlay = new JMenuItem ("How to play");
+    private JMenuItem controls = new JMenuItem("Controls");
+    private JMenuItem about = new JMenuItem("About");
+
     // App version
     private final double VERSION = 1.0;
+
+    // Window size
+    private final int WINDOW_WIDTH = 700;
+    private final int WINDOW_HEIGHT = 550;
 
     // Custom fonts used for this program
     private static Font font1;
@@ -30,12 +41,48 @@ public class Window extends JPanel
         // Create the main window
         gameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         gameFrame.setTitle("Snake");
-        gameFrame.setSize(700, 550);
+        gameFrame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         gameFrame.setResizable(false);
         gameFrame.add(new Board());
-        JMenuBar bar = new JMenuBar();
-        JMenu gameMenu = new JMenu("Game");
+        loadMenu();
+        loadFonts();
+        gameFrame.setVisible(true);
+    }
 
+    public void loadMenu()
+    {
+        howToPlay.addActionListener(e -> JOptionPane.showMessageDialog(gameFrame, "To play the game use the arrow keys to move the snake" +
+                        " around and eat the apples." +
+                        " \nThe bigger your snake the higher your score! " +
+                        "\nWatch out for walls and your tail, hit these and the game is over!",
+                "How to play",JOptionPane.INFORMATION_MESSAGE));
+        controls.addActionListener(e -> JOptionPane.showMessageDialog(gameFrame,
+                "Up Key: Move Up\n" +
+                        "Left Key: Move Left\n" +
+                        "Right Key: Move Right\n" +
+                        "Down Key: Move Down\n" +
+                        "'n': New Game\n" +
+                        "'p': Pause Screen\n" +
+                        "'c': Exit Pause Screen\n" +
+                        "Esc: Close window","Controls", JOptionPane.INFORMATION_MESSAGE));
+        about.addActionListener(e -> JOptionPane.showMessageDialog(gameFrame, "Snake: version " + VERSION +
+                        "\nwritten by Joshua Torres.\n" +
+                        "Victoria College\n" +
+                        "COSC 1436 Spring 2017 Final Project\n"
+                , "About", JOptionPane.PLAIN_MESSAGE));
+
+        // Add helpmenu elements
+        helpMenu.add(howToPlay);
+        helpMenu.add(controls);
+        helpMenu.add(about);
+
+        // Add game and help menu bar
+        bar.add(helpMenu);
+        gameFrame.setJMenuBar(bar);
+    }
+
+    public void loadFonts()
+    {
         // Initialize fonts
         try {
             // Create the font to use. Specify the size!
@@ -51,53 +98,6 @@ public class Window extends JPanel
             e.printStackTrace();
             System.out.println("Error: Font not found.");
         }
-
-        // New game button
-        JMenuItem newgame = new JMenuItem("New Game");
-        //newgame.addActionListener(e -> restartGame());
-
-        // create a jframe
-        JFrame frame = new JFrame("Example");
-
-        // Various other buttons
-        JMenu helpMenu = new JMenu("Help");
-        JMenuItem howToPlay = new JMenuItem ("How to play");
-        JMenuItem controls = new JMenuItem("Controls");
-        JMenuItem about = new JMenuItem("About");
-
-        howToPlay.addActionListener(e -> JOptionPane.showMessageDialog(frame, "To play the game use the arrow keys to move the snake" +
-                        " around and eat the apples." +
-                        " \nThe bigger your snake the higher your score! " +
-                        "\nWatch out for walls and your tail, hit these and the game is over!",
-                "How to play",JOptionPane.INFORMATION_MESSAGE));
-        controls.addActionListener(e -> JOptionPane.showMessageDialog(frame,
-                "Up Key: Move Up\n" +
-                        "Left Key: Move Left\n" +
-                        "Right Key: Move Right\n" +
-                        "Down Key: Move Down\n" +
-                        "'n': New Game\n" +
-                        "'p': Pause Screen\n" +
-                        "'c': Exit Pause Screen\n" +
-                        "Esc: Close window","Controls", JOptionPane.INFORMATION_MESSAGE));
-        about.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Snake: version " + VERSION +
-                        "\nwritten by Joshua Torres.\n" +
-                        "Victoria College\n" +
-                        "COSC 1436 Spring 2017 Final Project\n"
-                , "About", JOptionPane.PLAIN_MESSAGE));
-
-        // Add helpmenu elements
-        helpMenu.add(howToPlay);
-        helpMenu.add(controls);
-        helpMenu.add(about);
-
-        // Add gamemenu elements
-        gameMenu.add(newgame);
-
-        // Add game and help menu bar
-        bar.add(gameMenu);
-        bar.add(helpMenu);
-        gameFrame.setJMenuBar(bar);
-        gameFrame.setVisible(true);
     }
 
     public static JFrame getGameFrame()
